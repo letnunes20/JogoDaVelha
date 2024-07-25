2aad959e70312e9f74e83c2f8865ab58a166f05d
letnunes20 <letcanunes@gmail.com>
2024-07-25 11:38:53 -0300

    Criação do jogo da velha

---
+++ jogo-da-velha/components/figura.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+
+
+export default function Figura({vetor, posicao, clicado}) {
+    const getIcon = () => {
+        if (vetor[posicao] !== 0) {
+            return vetor[posicao] ? "cross" : "circle";
+        }
+        return "pencil";
+    };
+  return (
+    <TouchableOpacity onPress={clicado}>
+        <Entypo name={getIcon()} size={100} color="#000"/>
+    </TouchableOpacity>
+  )
+}
+
